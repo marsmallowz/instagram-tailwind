93a973b0052a10b5e5f1d8dcc3e35c09bf638ce1
marsmallowz <alsandymaulana15@gmail.com>
2022-12-21 14:05:35 +0700

    Change Config

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = "http://localhost:2000";
+export const API_URL = "https://silk-abounding-airmail.glitch.me";
 export const axiosInstance = axios.create({
   baseURL: API_URL,
 });
